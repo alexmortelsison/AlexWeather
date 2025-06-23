515a8b68f62cfe63f124fb9218f589821f170f4b
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-06-23 19:34:45 +0900

    updated manifest and plist

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,7 +1,24 @@
+import 'package:alexweather/components/main_card.dart';
+import 'package:alexweather/models/weather.dart';
+import 'package:alexweather/services/weather_service.dart';
 import 'package:flutter/material.dart';
 
-class WeatherPage extends StatelessWidget {
+class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  late Future<Weather> weatherFuture;
+  final weatherService = WeatherService();
+
+  @override
+  void initState() {
+    super.initState();
+    weatherFuture = weatherService.fetchWeather();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +36,34 @@ class WeatherPage extends StatelessWidget {
             end: Alignment.bottomRight,
             colors: [Colors.blue.shade400, Colors.blue.shade200],
           ),
+        ),
+        child: Column(
+          children: [
+            FutureBuilder(
+              future: weatherFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator.adaptive());
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text("Error:${snapshot.error}"),
+                  );
+                } else if (snapshot.hasData) {
+                  final weather = snapshot.data;
+                  return MainCard(
+                    cityName: weather!.cityName,
+                    temperature: "${weather.temperature}",
+                    weatherIcon: weather.weatherIconPath,
+                    weatherDescription: weather.weatherDescription,
+                  );
+                } else {
+                  return Center(
+                    child: Text("No data available."),
+                  );
+                }
+              },
+            ),
+          ],
         ),
       ),
     );
