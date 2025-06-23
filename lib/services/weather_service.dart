42bd21b82fc0fe336625a0f20bae5405c477c040
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-06-23 19:00:15 +0900

    added weather service

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,7 +1,41 @@
+import 'dart:convert';
+
 import 'package:alexweather/models/weather.dart';
+import 'package:alexweather/secrets.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 class WeatherService {
   Future<Weather> fetchWeather() async {
-    bool serviceEnabled = 
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      throw Exception("Location services are disabled.");
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+    if (permission == LocationPermission.denied) {
+      throw Exception("Location permissions are denied.");
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+
+    final lat = position.latitude;
+    final lon = position.longitude;
+    final url = Uri.parse(
+      "https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$apiKey&units=metric",
+    );
+
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = jsonDecode(response.body);
+      return Weather.fromJson(data);
+    } else {
+      throw Exception("Failed to fetch weather data.");
+    }
   }
 }
